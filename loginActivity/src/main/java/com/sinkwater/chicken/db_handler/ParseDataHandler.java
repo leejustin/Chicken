d244fb9397799e261c80100d2650489a2d235e35
Justin <jlee@novacoast.com>
2014-11-28 21:46:58 -0800

    Adding files that I forgot to add because I'm an idiot

---
+++ loginActivity/src/main/java/com/sinkwater/chicken/db_handler/ParseDataHandler.java
@@ -0,0 +1,11 @@
+package com.sinkwater.chicken.db_handler;
+
+/**
+ * Created by justin on 11/16/14.
+ */
+public class ParseDataHandler {
+
+
+
+    //Need function to get data about an organization.
+}
