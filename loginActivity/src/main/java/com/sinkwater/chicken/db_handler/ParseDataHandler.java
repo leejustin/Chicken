afbbb0a190a1009950d494b41931ab633d5f3649
Justin <jlee@novacoast.com>
2014-12-01 00:22:51 -0800

    Save organization object into database.

--- loginActivity/src/main/java/com/sinkwater/chicken/db_handler/ParseDataHandler.java
+++ loginActivity/src/main/java/com/sinkwater/chicken/db_handler/ParseDataHandler.java
@@ -1,5 +1,10 @@
 package com.sinkwater.chicken.db_handler;
 
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 /**
  * Created by justin on 11/16/14.
  */
@@ -7,5 +12,24 @@ public class ParseDataHandler {
 
 
 
+
     //Need function to get data about an organization.
+
+
+
+
+    //Set Parse organization information
+  //  ParseQuery<ParseObject> query = ParseQuery.getQuery("")
+    public void createOrg(String orgName, String generalId, double latitude, double longitude) {
+
+        //Set up data on organization
+        ParseObject orgData = new ParseObject("Organization");
+        orgData.put("name", orgName);
+        orgData.put("generalId", generalId);
+        orgData.put("latitude", latitude);
+        orgData.put("longitude", longitude);
+
+        orgData.saveInBackground();
+    }
+
 }
