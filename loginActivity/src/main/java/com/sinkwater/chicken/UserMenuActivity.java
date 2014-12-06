db11adefd1d741eeac4c016f91a6dd304a643e2e
Justin <jlee@novacoast.com>
2014-12-06 15:29:46 -0800

    Set up skeleton code and template for user menu

--- loginActivity/src/main/java/com/sinkwater/chicken/UserMenuActivity.java
+++ loginActivity/src/main/java/com/sinkwater/chicken/UserMenuActivity.java
@@ -1,40 +1,199 @@
 package com.sinkwater.chicken;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import java.util.List;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+
+import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.ProfilePictureView;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+import com.parse.*;
 
 
 public class UserMenuActivity extends Activity {
+
+    ListView listview;
+    List<ParseObject> ob;
+    ProgressDialog mProgressDialog;
+    ArrayAdapter<String> adapter;
+
+    private ProfilePictureView userProfilePictureView;
+    private TextView userNameView;
+    private TextView userGenderView;
+    private TextView userEmailView;
+    private Button logoutButton;
+    private Button setButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.usermenu);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usermenu);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+       // setParseData();
+
+        userProfilePictureView = (ProfilePictureView) findViewById(R.id.userProfilePicture);
+        userNameView = (TextView) findViewById(R.id.userName);
+        //userGenderView = (TextView) findViewById(R.id.userGender);
+        //userEmailView = (TextView) findViewById(R.id.userEmail);
+
+        logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLogoutButtonClicked();
+            }
+        });
+
+        // Fetch Facebook user info if the session is active
+        Session session = ParseFacebookUtils.getSession();
+        if (session != null && session.isOpened()) {
+            makeMeRequest();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void setParseData() {
+        //nothing lol
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            // Check if the user is currently logged
+            // and show any cached content
+            updateViewsWithProfileInfo();
+        } else {
+            // If the user is not logged in, go to the
+            // activity showing the login view.
+            startLoginActivity();
+        }
+    }
+
+    private void makeMeRequest() {
+        Request request = Request.newMeRequest(ParseFacebookUtils.getSession(),
+                new Request.GraphUserCallback() {
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        if (user != null) {
+                            // Create a JSON object to hold the profile info
+                            JSONObject userProfile = new JSONObject();
+                            try {
+                                // Populate the JSON object
+                                userProfile.put("facebookId", user.getId());
+                                userProfile.put("name", user.getName());
+                                if (user.getProperty("gender") != null) {
+                                    userProfile.put("gender", (String) user.getProperty("gender"));
+                                }
+                                if (user.getProperty("email") != null) {
+                                    userProfile.put("email", (String) user.getProperty("email"));
+                                }
+
+                                // Save the user profile info in a user property
+                                ParseUser currentUser = ParseUser.getCurrentUser();
+                                currentUser.put("profile", userProfile);
+                                currentUser.saveInBackground();
+
+                                // Show the user info
+                                updateViewsWithProfileInfo();
+                            } catch (JSONException e) {
+                                Log.d(FacebookHandler.TAG, "Error parsing returned user data. " + e);
+                            }
+
+                        } else if (response.getError() != null) {
+                            if ((response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_RETRY) ||
+                                    (response.getError().getCategory() == FacebookRequestError.Category.AUTHENTICATION_REOPEN_SESSION)) {
+                                Log.d(FacebookHandler.TAG, "The facebook session was invalidated." + response.getError());
+                                onLogoutButtonClicked();
+                            } else {
+                                Log.d(FacebookHandler.TAG,
+                                        "Some other error: " + response.getError());
+                            }
+                        }
+                    }
+                }
+        );
+        request.executeAsync();
+    }
+
+
+    private void updateViewsWithProfileInfo() {
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        if (currentUser.has("profile")) {
+            JSONObject userProfile = currentUser.getJSONObject("profile");
+            try {
+
+                if (userProfile.has("facebookId")) {
+                    userProfilePictureView.setProfileId(userProfile.getString("facebookId"));
+                } else {
+                    // Show the default, blank user profile picture
+                    userProfilePictureView.setProfileId(null);
+                }
+
+                if (userProfile.has("name")) {
+                    userNameView.setText(userProfile.getString("name"));
+                } else {
+                    userNameView.setText("");
+                }
+				/*
+				if (userProfile.has("gender")) {
+					userGenderView.setText(userProfile.getString("gender"));
+				} else {
+					userGenderView.setText("");
+				}
+
+				if (userProfile.has("email")) {
+					userEmailView.setText(userProfile.getString("email"));
+				} else {
+					userEmailView.setText("");
+				}
+				*/
+            } catch (JSONException e) {
+                Log.d(FacebookHandler.TAG, "Error parsing saved user data.");
+            }
+        }
+    }
+
+    private void onLogoutButtonClicked() {
+        // Log the user out
+        ParseUser.logOut();
+
+        // Go to the login view
+        startLoginActivity();
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
